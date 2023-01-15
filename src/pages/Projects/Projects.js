5e3b5c9634c5f3f8d103a8accfef68eaee8c7947
myspace8 <comfyspace8@gmail.com>
2023-01-14 18:51:00 -0800

    init

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,11 +1,36 @@
-// import { Outlet } from "react-router";
 import "./Projects.scss";
 
 export default function Projects() {
   return (
     <div>
-      <h1>Hello Projects</h1>
-      {/* <Outlet /> */}
+      <div className="card-container">
+        <div className="card">
+          <div className="media-img">
+            <img src={"images/avatar.JPG"} width={300} height={250} alt="" />
+          </div>
+          <div className="media-desc">
+            <h3>{"Quiz App"}</h3>
+            <p>{"I fucused on implementing 'timer' as a functionality."}</p>
+          </div>
+          <div className="links">
+            <a href="/">View Project</a> 
+            <a href="/">Source Code</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="media-img">
+            <img src={"images/avatar.JPG"} width={300} height={250} alt="" />
+          </div>
+          <div className="media-desc">
+            <h3>{"SpaceX Website Clone"}</h3>
+            <p>{'SpaceX'}</p>
+          </div>
+          <div className="links">
+            <a href="/">View Project</a>
+            <a href="/">Source Code</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
